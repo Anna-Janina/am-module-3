fb4467e835863fcd6dbf354dfffdabcfc13ef602
Anna <annajaninaschmidt@gmail.com>
2022-12-04 12:52:30 +1100

    first function

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+var specChars = ['@', '!']
+var numChars = ['0']
+var lowercaseChars = ['a']
+var uppercaseChars = ['A']
+
+// function to get users password options - this function should return object of users selections
+function getUserPasswordOptions() {
+  var passwordLength = parseInt(prompt('how many chars'), 10)
+
+  if (passwordLength < 8) {
+    alert('must be longer than blah')
+    return;
+  }
+  // if statement for too long 
+
+  var hasSpechChars = confirm('click OK to use spec chars')
+
+  var usersPasswordOptions = {
+    passwordLength: passwordLength,
+    hasSpechChars: hasSpechChars,
+  }
+
+  return usersPasswordOptions
+}
+
+
+// function to get random array element 
+function getRandomArrayElement(array) {
+  var randomIndex = Math.floor(Math.random() * array.length)
+  var randomElement = array[randomIndex]
+
+  return randomElement
+}
+// write generate password function that takes our input and generates password accordingly
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
